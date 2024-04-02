ff6298662295a7ba6ae9961db0be5de58ba62287
Sienna <sgoldenbird@gmail.com>
2024-04-02 19:28:36 +0900

    design: #8- sign up design

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,4 +1,39 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { RiKakaoTalkFill } from "react-icons/ri";
+import { SiNaver } from "react-icons/si";
+import { FcGoogle } from "react-icons/fc";
+
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return (
+    <SignInContainer>
+      <form>
+        <h1>Sign In</h1>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <Link>Forgot Password</Link>
+        <button>로그인</button>
+        <div className="sns-icons">
+          <Link>
+            <SiNaver />
+          </Link>
+          <Link>
+            <RiKakaoTalkFill />
+          </Link>
+          <Link>
+            <FcGoogle />
+          </Link>
+        </div>
+      </form>
+      <div className="toggle-to-signup">
+        <span>Don&apos;t have an Account?</span>
+        <button>Sign Up</button>
+      </div>
+    </SignInContainer>
+  );
 };
 export default SignIn;
+
+const SignInContainer = styled.div`
+  background-color: ;
+`;
