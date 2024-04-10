0e6e6a9b6af5b6c1bafae821b1a5cb904b650ddd
Hyo Bin <a032875@gmail.com>
2024-04-10 13:16:53 +0900

    chore:#10-recoil persist 추가

--- src/recoil/RecoilWastes.jsx
+++ src/recoil/RecoilWastes.jsx
@@ -1,8 +1,8 @@
 import { atom } from "recoil";
-// import { recoilPersist } from "recoil-persist";
-// const { persistAtom } = recoilPersist();
+import { recoilPersist } from "recoil-persist";
+const { persistAtom } = recoilPersist();
 export const postsState = atom({
   key: "postsState",
   default: [],
-  // effects_UNSTABLE: [persistAtom],
+  effects_UNSTABLE: [persistAtom],
 });
