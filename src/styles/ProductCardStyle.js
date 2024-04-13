34d67db67c5590e6ecb3c01ef9558e94c17d7b95
Hyo Bin <a032875@gmail.com>
2024-04-13 16:24:16 +0900

    design:#21-목록페이지 디자인 수정

--- src/styles/ProductCardStyle.js
+++ src/styles/ProductCardStyle.js
@@ -1,14 +1,16 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  display: grid;
+  /* display: grid;
   grid-template-columns: 20rem 20rem 20rem;
   gap: 70px;
-  justify-content: center;
+  justify-content: center; */
   .card {
     border: 1px solid var(--grey-box);
     padding: 1.5rem;
     box-sizing: border-box;
+    box-shadow: 5px 5px 8px var(--grey-box);
+    border-radius: 10px;
     .product-box {
       display: flex;
       flex-direction: column;
@@ -49,34 +51,22 @@ export const Container = styled.div`
       display: flex;
       justify-content: flex-end;
       align-items: center;
-      gap: 1.5rem;
+      gap: 1rem;
       font-size: 1.5rem;
-      p {
+      .like {
+        display: flex;
+
+        gap: 0.3rem;
+        button {
+          border: none;
+          background-color: white;
+          cursor: pointer;
+        }
       }
     }
   }
-  /* @media (max-width: 1440px) {
-    display: grid;
-    grid-template-columns: 22rem 22rem 22rem;
-    gap: 50px;
-    justify-content: center;
-    .card {
-      padding: 1.5em;
-      .product-box {
-        display: flex;
-        flex-direction: cloumn;
-      }
-      .sell-status {
-        float: right;
-        font-size: 1.5rem;
-      }
-    }
-  } */
+
   @media (max-width: 1280px) {
-    display: grid;
-    grid-template-columns: 19rem 19rem 19rem;
-    gap: 40px;
-    justify-content: center;
     .card {
       .product-box {
         display: flex;
@@ -108,9 +98,6 @@ export const Container = styled.div`
     }
   }
   @media (max-width: 1080px) {
-    display: grid;
-    grid-template-columns: 40rem;
-    gap: 70px;
     .card {
       border-bottom: 1px solid var(--grey-box);
       .product-box {
@@ -151,9 +138,6 @@ export const Container = styled.div`
     }
   }
   @media (max-width: 768px) {
-    display: grid;
-    grid-template-columns: 35rem;
-    gap: 70px;
     .card {
       border-bottom: 1px solid var(--grey-box);
       .product-box {
@@ -194,14 +178,4 @@ export const Container = styled.div`
       }
     }
   }
-  /* @media (max-width: 1280px) {
-    width: 62rem;
-  }
-  @media (max-width: 1024px) {
-    width: 50rem;
-  }
-  @media (max-width: 768px) {
-    width: 40rem;
-     font-size: 0.88rem; 
-  } */
 `;
