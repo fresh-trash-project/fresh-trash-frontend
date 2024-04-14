d600f396ff900ad529a74b462098c88b70bf8f1a
Hyo Bin <a032875@gmail.com>
2024-04-15 00:55:28 +0900

    chore:#29-eslint 수정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,5 @@
 module.exports = {
   root: true,
-  env: { browser: tue, es2020: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -12,7 +11,7 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'import'],
   rules: {
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
@@ -21,7 +20,7 @@ module.exports = {
     ],
     'no-var': 'warn', // var 금지
     'no-multiple-empty-lines': 'warn', // 여러 줄 공백 금지
-    'no-console': 'off', // console.log() 금지
+    'no-console': 'warn', // console.log() 금지
     eqeqeq: 'warn', // 일치 연산자 사용 필수
     'dot-notation': 'warn', // 가능하다면 dot notation 사용
     'no-unused-vars': 'warn', // 사용하지 않는 변수 금지
@@ -30,7 +29,7 @@ module.exports = {
     'react/no-direct-mutation-state': 'warn', // state 직접 수정 금지
     'react/jsx-no-useless-fragment': 'warn', // 불필요한 fragment 금지
     'react/no-unused-state': 'warn', // 사용되지 않는 state
-    'react/jsx-key': 'off', // 반복문으로 생성하는 요소에 key 강제
+    'react/jsx-key': 'warn', // 반복문으로 생성하는 요소에 key 강제
     'react/self-closing-comp': 'warn', // 셀프 클로징 태그 가능하면 적용
     'react/jsx-curly-brace-presence': 'off', // jsx 내 불필요한 중괄호 금지
     'prettier/prettier': [
