d6002f1286189f4d64833c6b2e01fa2daae5249c
Hyo Bin <a032875@gmail.com>
2024-04-15 21:11:06 +0900

    feat:#33-상세페이지 기능 추가
    html,css,모달 , 반응형 추가

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -56,8 +56,7 @@ const ProductCard = ({ post, onDelete }) => {
             {post.wastePrice}원
           </span>
           <Link
-            to="/"
-            href="#"
+            to={`/${post.id}`}
             className="text-white bg-green-900 hover:bg-green-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center  "
           >
             상세보기
--- src/router.jsx
+++ src/router.jsx
@@ -1,37 +1,37 @@
-import { createBrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
-import Home from "./pages/Home";
-import SignUp from "./pages/SignUp";
-import SignIn from "./pages/SignIn";
-import MyPage from "./pages/MyPage";
-import MyTradeList from "./pages/MyTradeList";
-import MyAuctionList from "./pages/MyAuctionList";
-import MyLikes from "./pages/MyLikes";
-import ProductsList from "./pages/ProductsList";
-import ProductDetail from "./pages/ProductDetail";
-import ProductAdd from "./pages/ProductAdd.jsx";
-import ProductEdit from "./pages/ProductEdit.jsx";
-import AuctionList from "./pages/AuctionList";
-import AuctionDetail from "./pages/AuctionDetail";
-import Chat from "./pages/Chat";
-import ChatList from "./pages/ChatList";
-import Pay from "./pages/Pay";
+import { createBrowserRouter } from 'react-router-dom';
+import App from './App.jsx';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import MyPage from './pages/MyPage';
+import MyTradeList from './pages/MyTradeList';
+import MyAuctionList from './pages/MyAuctionList';
+import MyLikes from './pages/MyLikes';
+import ProductsList from './pages/ProductsList';
+import ProductDetail from './pages/ProductDetail';
+import ProductAdd from './pages/ProductAdd.jsx';
+import ProductEdit from './pages/ProductEdit.jsx';
+import AuctionList from './pages/AuctionList';
+import AuctionDetail from './pages/AuctionDetail';
+import Chat from './pages/Chat';
+import ChatList from './pages/ChatList';
+import Pay from './pages/Pay';
 
 export const router = createBrowserRouter([
-  { path: "/", element: <App />, errorElement: <div>404 Not Found</div> },
-  { path: "/SignUp", element: <SignUp /> },
-  { path: "/SignIn", element: <SignIn /> },
-  { path: "/MyPage", element: <MyPage /> },
-  { path: "/MyTradeList", element: <MyTradeList /> },
-  { path: "/MyAuctionList", element: <MyAuctionList /> },
-  { path: "/MyLikes", element: <MyLikes /> },
-  { path: "/ProductsList", element: <ProductsList /> },
-  { path: "/ProductDetail", element: <ProductDetail /> },
-  { path: "/ProductAdd", element: <ProductAdd /> },
-  { path: "/ProductEdit", element: <ProductEdit /> },
-  { path: "/AuctionList", element: <AuctionList /> },
-  { path: "/AuctionDetail", element: <AuctionDetail /> },
-  { path: "/Chat", element: <Chat /> },
-  { path: "/ChatList", element: <ChatList /> },
-  { path: "/Pay", element: <Pay /> },
+  { path: '/', element: <App />, errorElement: <div>404 Not Found</div> },
+  { path: '/SignUp', element: <SignUp /> },
+  { path: '/SignIn', element: <SignIn /> },
+  { path: '/MyPage', element: <MyPage /> },
+  { path: '/MyTradeList', element: <MyTradeList /> },
+  { path: '/MyAuctionList', element: <MyAuctionList /> },
+  { path: '/MyLikes', element: <MyLikes /> },
+  { path: '/ProductsList', element: <ProductsList /> },
+  { path: '/:id', element: <ProductDetail /> },
+  { path: '/ProductAdd', element: <ProductAdd /> },
+  { path: '/ProductEdit', element: <ProductEdit /> },
+  { path: '/AuctionList', element: <AuctionList /> },
+  { path: '/AuctionDetail', element: <AuctionDetail /> },
+  { path: '/Chat', element: <Chat /> },
+  { path: '/ChatList', element: <ChatList /> },
+  { path: '/Pay', element: <Pay /> },
 ]);
