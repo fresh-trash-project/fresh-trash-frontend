20fce4a7ebe592ec7ac16f27772787a4a2f29597
Sienna <sgoldenbird@gmail.com>
2024-04-15 18:59:52 +0900

    feat: #27-Create MyPage with tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 import { RecoilRoot } from 'recoil';
+import 'tailwindcss/tailwind.css';
+import 'daisyui/dist/full.css';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RecoilRoot>
