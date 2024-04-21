854b85287dc99d935092a985ddc6aa5b30ab3853
Hyo Bin <a032875@gmail.com>
2024-04-22 01:57:45 +0900

    chore:#45-소켓 설정 주석화

--- src/components/chat/Room.jsx
+++ src/components/chat/Room.jsx
@@ -1,23 +1,23 @@
 import React, { useEffect } from 'react';
 import { useState } from 'react';
 import InputField from './InputField';
-import socket from '../../api/chat/server';
+// import socket from '../../api/chat/server';
 import MessageContainer from './MessageContainer';
 const Room = () => {
-  const [message, setMessage] = useState('');
-  const [messageList, setMessageList] = useState([]);
-  useEffect(() => {
-    socket.on('message', message => {
-      setMessageList(prevState => prevState.concat(message));
-      console.log('res', message);
-    });
-  }, []);
-  const sendMessage = event => {
-    event.preventDefault();
-    socket.emit('sendMessage'.message, res => {
-      console.log('sendMessage res', res);
-    });
-  };
+  // const [message, setMessage] = useState('');
+  // const [messageList, setMessageList] = useState([]);
+  // useEffect(() => {
+  //   socket.on('message', message => {
+  //     setMessageList(prevState => prevState.concat(message));
+  //     console.log('res', message);
+  //   });
+  // }, []);
+  // const sendMessage = event => {
+  //   event.preventDefault();
+  //   socket.emit('sendMessage'.message, res => {
+  //     console.log('sendMessage res', res);
+  //   });
+  // };
   return (
     <div>
       <InputField
