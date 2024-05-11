3e8e90c39c1e8cf3114a283a03f0b0746255970d
Hyo Bin <a032875@gmail.com>
2024-05-12 02:26:30 +0900

    feat:#73-파일에 url전역변수들 정리

---
+++ variable.js
@@ -0,0 +1,9 @@
+export const globalAPI = import.meta.env.VITE_API_URL + '/api/v1';
+export const globalAuthAPI = `${globalAPI}/auth`;
+export const globalOauthAPI = `${globalAPI}/oauth2/authorization`;
+export const globalMailAPI = `${globalAPI}/mail`;
+export const globalWatesAPI = `${globalAPI}/wastes`;
+export const globalAuctionsAPI = `${globalAPI}/auctions`;
+export const globalTransactionsApi = `${globalAPI}/transactions`;
+export const globalMembersAPI = `${globalAPI}/members`;
+export const globalAPINotisAPI = `${globalAPI}/notis`;
