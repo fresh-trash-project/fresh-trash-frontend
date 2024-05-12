0a516148d8634969316815fab40343652ee8aa80
Hyo Bin <a032875@gmail.com>
2024-05-12 19:12:01 +0900

    variable파일 url변경

--- variable.js
+++ variable.js
@@ -1,6 +1,7 @@
 export const globalAPI = import.meta.env.VITE_API_URL + '/api/v1';
 export const globalAuthAPI = `${globalAPI}/auth`;
-export const globalOauthAPI = `${globalAPI}/oauth2/authorization`;
+export const globalOauthAPI =
+  import.meta.env.VITE_API_URL + 'oauth2/authorization';
 export const globalMailAPI = `${globalAPI}/mail`;
 export const globalWatesAPI = `${globalAPI}/wastes`;
 export const globalAuctionsAPI = `${globalAPI}/auctions`;
