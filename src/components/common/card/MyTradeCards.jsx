b441633e1aa0ba660674fdbc23eb2d18f9d15a20
Hyo Bin <a032875@gmail.com>
2024-05-29 00:53:43 +0900

    refactor:#86-관심 페이지네이션 및 카테고리 추가
    카테고리는 쿼리만 추가 적용할 예정

--- src/components/common/card/MyTradeCards.jsx
+++ src/components/common/card/MyTradeCards.jsx
@@ -2,14 +2,8 @@ import ProductCard from './ProductCard';
 
 const MyTradeCards = ({ myList }) => {
   return (
-    <div className=" pt-4 px-20 lg:pt-5 pb-4 lg:pb-8 lg:px-36 xl:px-40 xl:container mx-auto 2xl:px-60 ">
-      <div className=" pt-2 lg:pt-4 pb-4 lg:pb-8 px-4 xl:px-2 mb-20 xl:container mx-auto">
-        <div className="grid gap-6 justify-items-center md:grid-cols-2  lg:grid-cols-3 item_ list ">
-          {myList.map(wastes => (
-            <ProductCard key={wastes.id} wastes={wastes} />
-          ))}
-        </div>
-      </div>
+    <div>
+      <ProductCard key={myList.id} wastes={myList} />
     </div>
   );
 };
