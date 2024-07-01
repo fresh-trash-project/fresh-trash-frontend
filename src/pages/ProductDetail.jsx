6dca6a4acc759f5b0c48d865844ac0c5d0bba35f
Hyo Bin <a032875@gmail.com>
2024-07-01 18:00:17 +0900

    chore:#109-오타수정

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Header from '../components/common/header/Header';
 import Footer from '../components/common/footer/Footer';
-import DetailPrduct from '../components/product/DetailProduct';
+import DetailProduct from '../components/product/DetailProduct';
 const ProductDetail = () => {
   return (
     <div>
       <Header />
-      <DetailPrduct />
+      <DetailProduct />
       <Footer />
     </div>
   );
