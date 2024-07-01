1e9da1cabd95920eaf6dccc0a49d5bb20dbc0ebd
Hyo Bin <a032875@gmail.com>
2024-07-01 22:52:16 +0900

    refactor:#113-카테고리 컴포넌트화

--- src/components/common/header/ListNav.jsx
+++ src/components/common/header/ListNav.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { FaPlus } from 'react-icons/fa6';
 import { FaSearch } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import CategoryDropDown from '../category/CategoryDropDown';
 const ListNav = ({
   handleCategoryChange,
   isSearchVisible,
@@ -16,52 +17,7 @@ const ListNav = ({
   return (
     <div className="navbar flex-row justify-between bg-base-100 shadow-md">
       <div className="flex">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn m-1">
-            카테고리
-          </div>
-          <ul
-            tabIndex={0}
-            className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li onClick={() => handleCategoryChange('전체')}>
-              <p>전체</p>
-            </li>
-            <li onClick={() => handleCategoryChange('ELECTRONICS')}>
-              <p>전자기기</p>
-            </li>
-            <li onClick={() => handleCategoryChange('CLOTHING')}>
-              <p>의류</p>
-            </li>
-            <li onClick={() => handleCategoryChange('HOME_KITCHEN')}>
-              <p>생활/주방</p>
-            </li>
-            <li onClick={() => handleCategoryChange('BEAUTY')}>
-              <p>뷰티</p>
-            </li>
-            <li onClick={() => handleCategoryChange('HEALTH')}>
-              <p>건강</p>
-            </li>
-            <li onClick={() => handleCategoryChange('SPORTS')}>
-              <p>스포츠</p>
-            </li>
-            <li onClick={() => handleCategoryChange('BOOKS')}>
-              <p>도서</p>
-            </li>
-            <li onClick={() => handleCategoryChange('TOYS_GAMES')}>
-              <p>장난감/게임</p>
-            </li>
-            <li onClick={() => handleCategoryChange('FURNITURE_DECOR')}>
-              <p>가구/인테리어</p>
-            </li>
-            <li onClick={() => handleCategoryChange('PET_SUPPLIES')}>
-              <p>반려동물용품</p>
-            </li>
-            <li onClick={() => handleCategoryChange('PLANT_SUPPLIES')}>
-              <p>식물</p>
-            </li>
-          </ul>
-        </div>
+        <CategoryDropDown handleCategoryChange={handleCategoryChange} />
       </div>
       <div className="flex">
         <div
