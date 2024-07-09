46b50fca7d5c8f7bcca3a58f7458c6852cb66dbd
Sienna <sgoldenbird@gmail.com>
2024-07-09 18:03:52 +0900

    install i18n

--- src/components/common/header/Nav.jsx
+++ src/components/common/header/Nav.jsx
@@ -4,6 +4,7 @@ import { useRecoilState } from 'recoil';
 import { AlarmMsgState, AlarmState } from '../../../recoil/RecoilAlarm';
 import NavEndButton from '../button/NavEndButton';
 import Cookies from 'js-cookie';
+import { useTranslation } from 'react-i18next';
 
 const Nav = () => {
   const [signIn, setSignIn] = useRecoilState(signInState);
@@ -11,12 +12,17 @@ const Nav = () => {
   const [alarmOpen, setAlarmOpen] = useRecoilState(AlarmState);
   const [alarmMsg, setAlarmMsg] = useRecoilState(AlarmMsgState);
   const navigate = useNavigate();
+  const { t, i18n } = useTranslation();
 
   const menuItems = [
-    { path: '/', label: 'HOME' },
-    { path: '/ProductAdd', label: '애물단지 등록' },
-    { path: '/ProductsList', label: '애물단지 거래' },
-    { path: '/AuctionList', label: '애물단지 경매' },
+    { path: '/', label: t('HOME') },
+    { path: '/ProductAdd', label: t('ADD_PRODUCT') },
+    { path: '/ProductsList', label: t('PRODUCTS_LIST') },
+    { path: '/AuctionList', label: t('AUCTION_LIST') },
+    // { path: '/', label: 'HOME' },
+    // { path: '/ProductAdd', label: '애물단지 등록' },
+    // { path: '/ProductsList', label: '애물단지 거래' },
+    // { path: '/AuctionList', label: '애물단지 경매' },
   ];
 
   const handleLogout = () => {
@@ -68,7 +74,21 @@ const Nav = () => {
       </div>
 
       {/*Nav바 오른쪽 구성 ------------------------------------------------------------------------------------------------- */}
-      <div className="navbar-end ">
+      <div className="navbar-end flex flex-col items-end -space-y-7 md:-space-y-4 lg:-space-y-1">
+        <div className="flex -space-x-3 md:-space-x-1 lg:space-x-1">
+          <button
+            className="btn btn-ghost btn-circle text-xs md:text-[1rem] lg:text-lg justify-end"
+            onClick={() => i18n.changeLanguage('en')}
+          >
+            ENG
+          </button>
+          <button
+            className="btn btn-ghost btn-circle text-xs md:text-[1rem] lg:text-lg justify-end"
+            onClick={() => i18n.changeLanguage('ko')}
+          >
+            KOR
+          </button>
+        </div>
         {!signIn ? (
           <div className="flex ">
             {/* 로그인 버튼  */}
